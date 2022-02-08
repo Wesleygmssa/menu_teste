da256eedb6bfdbfc5386ee83c85bc769134f939c
Guerra <wesley.moreira@vetta.digital>
2022-02-08 00:38:38 -0300

    Chore: Adjustment menu

---
+++ src/components/menu/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  width: 220px;
+`;
+
+export const ContainerInte = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+
+  .Checkbox {
+    flex: 1;
+  }
+  .title {
+    flex: 1;
+  }
+
+ 
+`;
